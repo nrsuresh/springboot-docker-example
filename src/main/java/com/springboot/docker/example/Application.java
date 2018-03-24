b78c903a877115098ad3b3fca90efe9eb07e724a
Suresh Raghavan <contactnrsuresh@gmail.com>
2018-03-24 11:32:07 -0500

    Initial commit

---
+++ src/main/java/com/springboot/docker/example/Application.java
@@ -0,0 +1,13 @@
+package com.springboot.docker.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/springboot/docker/example/services/MessageService.java
@@ -0,0 +1,5 @@
+package com.springboot.docker.example.services;
+
+public interface MessageService {
+    String message(final String name);
+}
